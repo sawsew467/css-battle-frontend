c86d01a98faf6004c9c59a2f1d0014f85926e6f5
sawsew467 <bao.thang.1912@gmail.com>
2023-04-28 01:21:44 +0700

    fix router

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import Logo from "../assets/images/header/logo.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { logout } from "../redux/slices/currentUser";
+import { useNavigate } from "react-router";
 
 interface IProps {
   setIsShowLoginModal?: React.Dispatch<React.SetStateAction<boolean>>;
@@ -24,6 +25,10 @@ function Header({ setIsShowLoginModal }: IProps) {
     dispatch(logout());
     window.location.reload();
   };
+  const navigate = useNavigate();
+  const handleManagementClick = () => {
+    navigate("/admin");
+  };
   return (
     <div className="w-[100vw] h-[64px] px-4 bg-black flex items-center justify-between">
       <a href="/">
@@ -31,22 +36,33 @@ function Header({ setIsShowLoginModal }: IProps) {
       </a>
       {currentUser.username ? (
         <div
-          className="relative z-30 bg-slate-700 px-3 py-2 flex flex-row items-center gap-3 rounded-md cursor-pointer hover:bg-slate-600 transition-all"
+          className="relative z-30 bg-slate-700 px-3 py-2 flex flex-row items-center rounded-md cursor-pointer hover:bg-slate-600 transition-all"
           onClick={() => setIsShow(!isShow)}
         >
           <img
             className="w-7 h-7 rounded-full object-fit"
             src={currentUser.avatarUrl}
           ></img>
-          <p className="text-white font-semibold">{currentUser.username}</p>
+          <p className="text-white font-semibold mx-3">
+            {currentUser.username}
+          </p>
           <i className="fa-sharp fa-solid fa-caret-down text-slate-400"></i>
           {isShow && (
-            <div
-              onClick={handleLogoutClick}
-              className="absolute z-20 right-0 top-[48px] h-[44px] bg-slate-700 px-3 py-2 flex flex-row items-center gap-3 rounded-md cursor-pointer hover:bg-slate-600 transition-all"
-            >
-              <p className="text-white font-semibold">Log out</p>
-              <i className="fa-solid fa-right-from-bracket text-slate-400"></i>
+            <div className="flex flex-col items-end absolute z-20 right-0 top-[48px] rounded-md">
+              <div
+                onClick={handleManagementClick}
+                className=" h-[44px] w-full bg-slate-700 px-3 py-2 flex flex-row items-center gap-3 rounded-tl-md rounded-tr-md cursor-pointer hover:bg-slate-600 transition-all"
+              >
+                <p className="text-white font-semibold">Management</p>
+                <i className="fa-solid fa-right-from-bracket text-slate-400"></i>
+              </div>
+              <div
+                onClick={handleLogoutClick}
+                className=" h-[44px] w-full bg-slate-700 px-3 py-2 flex flex-row justify-end items-center gap-3 rounded-bl-md rounded-br-md cursor-pointer hover:bg-slate-600 transition-all"
+              >
+                <p className="text-white font-semibold">Log out</p>
+                <i className="fa-solid fa-right-from-bracket text-slate-400"></i>
+              </div>
             </div>
           )}
         </div>
